b43834429e6f31dcd2e397aa9906492ed712279d
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-21 10:19:33 +0300

    feat: add superuser and adminModels

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','price',]
+    prepopulated_fields = {'slug': ('title',)}
+
+    def get_category(self):
+        print(self)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['cat_name']
+    prepopulated_fields = {'slug': ('cat_name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
