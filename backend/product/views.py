d2ceb317cf210f87eb6158b9e8c8c674f01b95fd
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-23 13:08:52 +0300

    style: ProductView

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ProductSerializer
-from .models import Product
+from .models import Product, Category
 
 
 class ProductView(viewsets.ModelViewSet):
-    serializer_class = ProductSerializer
-    
     queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
 
