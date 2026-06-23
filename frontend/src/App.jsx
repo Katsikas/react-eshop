094ca0de32d3815fc7528e5b19aef2b5071e14a7
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-23 15:33:30 +0300

    feat: add EmptyCart route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
 import Cart from "./pages/Cart";
+import EmptyCartPage from "./pages/EmptyCartPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<ProductsPage />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/empty-cart" element={<EmptyCartPage />} />
     </Routes>
   );
 };
