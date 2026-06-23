c1598f85e0cfacd6f45971986b6ed58da09b228b
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-23 13:08:06 +0300

    chore: migrations and models

--- backend/product/models.py
+++ backend/product/models.py
@@ -7,7 +7,6 @@ class Category(models.Model):
     cat_name = models.CharField(max_length=250)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
-    
     class Meta:
             verbose_name_plural = 'Categories'
 
@@ -23,30 +22,25 @@ class Tag(models.Model):
 
 
 
-def get_default_cat():
-     cat = Category.objects.get(pk=1)
-     return cat
-
-
-class Product(models.Model):
+class Product(models.Model):   
     title = models.CharField(max_length=250)
     price = models.DecimalField(
         max_digits=10, 
         decimal_places=2,
-        validators=[MinValueValidator(0.01)]
+        validators=[MinValueValidator(1)]
     )
     description = models.CharField(max_length=500)
-    category = models.ManyToManyField(Category, default=get_default_cat)
+    category = models.ManyToManyField(Category, related_name='products')
     image = models.CharField(max_length=500, null=True, blank=True)
     rating = models.DecimalField(
        max_digits=2,
        decimal_places=1,
-       validators=[MinValueValidator(0), MaxValueValidator(5)]
+       validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    tags = models.ManyToManyField(Tag, blank=True, related_name='tag')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='tags')
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
 
 
     def __str__(self):
-        return f'{self.title} {self.tags}' 
+        return f'{self.title}' 
