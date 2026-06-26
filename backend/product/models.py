a8464ca0a461f2a8d9db8604154867795f38a286
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-26 15:19:31 +0300

    feat: order model and new product fields

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+import uuid
 
 
 class Category(models.Model):
@@ -21,7 +22,6 @@ class Tag(models.Model):
         return f'{self.caption}'
 
 
-
 class Product(models.Model):   
     title = models.CharField(max_length=250)
     price = models.DecimalField(
@@ -38,9 +38,46 @@ class Product(models.Model):
        validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
     tags = models.ManyToManyField(Tag, blank=True, related_name='tags')
+    stock = models.PositiveIntegerField(default=1)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
 
 
     def __str__(self):
         return f'{self.title}' 
+
+
+class Order(models.Model):
+    class StatusChoises(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=StatusChoises.choices, default=StatusChoises.PENDING)
+    products = models.ManyToManyField(Product, through="OrderItem", related_name="orders")
+
+
+    def __str__(self):
+        return f'Order {self.order_id} by {self.user.username}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+
+    @property
+    def item_subtotal(self):
+        return self.product.price + self.quantity
+    
+    def __str__(self):
+        return f'{self.quantity} x {self.product.title} in Order {self.order.order_id}'
+
