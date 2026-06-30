7beed3b4bfaf73ef7a1fd98d178c3d41e783348d
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-06-30 09:50:33 +0300

    fix: add related_name to orderItem order

--- backend/product/models.py
+++ backend/product/models.py
@@ -69,7 +69,7 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
@@ -80,4 +80,3 @@ class OrderItem(models.Model):
     
     def __str__(self):
         return f'{self.quantity} x {self.product.title} in Order {self.order.order_id}'
-
