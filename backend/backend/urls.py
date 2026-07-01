72fb092dfed9a5a931ce796ee7156be53021d32f
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-01 11:25:18 +0300

    build: /cart/ url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from product.views import ProductView, OrderView
+from product.views import ProductView, OrderView, CartItemView
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductView, basename='product')
 router.register(r'orders', OrderView, basename='order')
+router.register(r'cart', CartItemView, basename='cart')
 
 
 urlpatterns = [
