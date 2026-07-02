3aa56e5ecfeaac647b89c492f1fd94a921eddc75
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-02 14:25:08 +0300

    feat: fetch CartItems on Mount

--- frontend/src/context/CardContext.jsx
+++ frontend/src/context/CardContext.jsx
@@ -1,10 +1,24 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import { CartReducer, initialState } from "./CartReducer";
+import { fetchCartItems } from "../services/productsApi";
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [state, dispatch] = useReducer(CartReducer, initialState);
+
+  useEffect(() => {
+    async function fetchCart() {
+      try {
+        const data = await fetchCartItems();
+        dispatch({ type: "UPDATE_STATE", payload: data });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchCart();
+  }, []);
 
   return (
     <CartContext.Provider value={{ state, dispatch }}>
