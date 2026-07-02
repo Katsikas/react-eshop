4873e7726f28979f7d69791ba0d8b9d8f0ae0465
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-02 14:45:19 +0300

    fix: !item.product then only state remove

--- frontend/src/components/CartUI/CartPopover.jsx
+++ frontend/src/components/CartUI/CartPopover.jsx
@@ -1,4 +1,5 @@
 import { useCart } from "../../context/CardContext";
+import { deleteCartItem } from "../../services/productsApi";
 
 export default function CartPopover() {
   const { state, dispatch } = useCart();
@@ -6,13 +7,30 @@ export default function CartPopover() {
 
   const ItemsCount = state.cartItems.length;
 
+  async function handleRemoveFromCart(item) {
+    if (!item.product) {
+      dispatch({ type: "REMOVE_FROM_CART", payload: item.product });
+      return;
+    }
+
+    try {
+      const response = await deleteCartItem(item);
+
+      if (response.status === 204) {
+        dispatch({ type: "REMOVE_FROM_CART", payload: item.product });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div id="cart-popover" popover="auto">
       <strong className="cart-quantity">Your Cart ({ItemsCount})</strong>
 
       <ul className="order-list">
         {cartItems.map((item) => (
-          <li className="cart-item" key={item.id}>
+          <li className="cart-item" key={item.id ?? item.product}>
             <div className="cell-pic">
               <img src={item.image} alt={item.title} height={45} width={45} />
             </div>
@@ -26,9 +44,7 @@ export default function CartPopover() {
               <button
                 aria-label="remove item from cart"
                 title="Remove item from cart"
-                onClick={() =>
-                  dispatch({ type: "REMOVE_FROM_CART", payload: item.id })
-                }
+                onClick={() => handleRemoveFromCart(item)}
               >
                 <img src="delete.svg" />
               </button>
