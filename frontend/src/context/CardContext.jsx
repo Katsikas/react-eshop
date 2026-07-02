01061bc718001b39cf0869fc349c58ab2c774057
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-02 16:48:50 +0300

    feat: loading state

--- frontend/src/context/CardContext.jsx
+++ frontend/src/context/CardContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { CartReducer, initialState } from "./CartReducer";
 import { fetchCartItems } from "../services/productsApi";
 
@@ -6,22 +12,25 @@ const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [state, dispatch] = useReducer(CartReducer, initialState);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchCart() {
+      setLoading(true);
       try {
         const data = await fetchCartItems();
         dispatch({ type: "UPDATE_STATE", payload: data });
       } catch (err) {
         console.log(err);
       }
+      setLoading(false);
     }
 
     fetchCart();
   }, []);
 
   return (
-    <CartContext.Provider value={{ state, dispatch }}>
+    <CartContext.Provider value={{ state, dispatch, loading }}>
       {children}
     </CartContext.Provider>
   );
