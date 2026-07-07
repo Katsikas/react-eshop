6cfc298c4c4eefce20cd454f775a0835f88abe5d
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-07 18:51:58 +0300

    feat: FavoriteView

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,5 @@
+from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializers import ProductSerializer, OrderSerializer, CartItemSerializer
 from .models import Product, Order, CartItem
@@ -19,3 +21,8 @@ class CartItemView(viewsets.ModelViewSet):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
 
+
+
+class FavoriteView(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(is_favorite=True)
+    serializer_class = ProductSerializer
