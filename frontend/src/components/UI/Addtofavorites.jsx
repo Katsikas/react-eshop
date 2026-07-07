c21dc4262e13943103f74e7b6f61fe6065349e39
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-07 18:55:22 +0300

    feat: addToFavorite component

--- frontend/src/components/UI/Addtofavorites.jsx
+++ frontend/src/components/UI/Addtofavorites.jsx
@@ -1,13 +1,46 @@
-const AddToFavorites = () => {
-  return (
-    <img
-      src="/heart.svg"
-      height={25}
-      width={25}
-      alt="Favorite icon"
-      className="favorite-icon"
-    />
-  );
-};
+import { useContext } from "react";
+import { patchIsFavoriteProduct } from "../../services/productsApi";
+import ProductsContext from "../../context/ProductsContext";
 
-export default AddToFavorites;
+export default function AddToFavorites({ product }) {
+  const { setProducts } = useContext(ProductsContext);
+
+  async function handleAddFavorite() {
+    try {
+      const response = await patchIsFavoriteProduct(
+        product,
+        !product.is_favorite,
+      );
+
+      if (response.status === 200) {
+        setProducts((prev) => {
+          const productToUpdate = prev.find((p) => p.id === product.id);
+          const indexOfProduct = prev.indexOf(productToUpdate);
+
+          prev[indexOfProduct] = {
+            ...productToUpdate,
+            is_favorite: !product.is_favorite,
+          };
+
+          return [...prev];
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const imageSrc = product.is_favorite ? "/heart_filled.svg" : "/heart.svg";
+
+  return (
+    <button onClick={handleAddFavorite}>
+      <img
+        src={imageSrc}
+        height={25}
+        width={25}
+        alt="Favorite icon"
+        className="favorite-icon"
+      />
+    </button>
+  );
+}
