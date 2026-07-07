20233b3264bacaf07e6acf519493156f526fb713
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-07 18:53:01 +0300

    feat: register /favorite/ to router

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from product.views import ProductView, OrderView, CartItemView
+from product.views import ProductView, OrderView, CartItemView, FavoriteView
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductView, basename='product')
 router.register(r'orders', OrderView, basename='order')
 router.register(r'cart', CartItemView, basename='cart')
+router.register(r'favorite', FavoriteView, basename='favorite')
 
 
 urlpatterns = [
