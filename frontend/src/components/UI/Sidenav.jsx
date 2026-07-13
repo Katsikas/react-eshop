082bddfbafe7c97ae768922d10ae9191e1993c39
Panagiotis Katsikadamas <pkatsikadamas@gmail.com>
2026-07-13 19:29:15 +0300

    feat: add links to sidenav

--- frontend/src/components/UI/Sidenav.jsx
+++ frontend/src/components/UI/Sidenav.jsx
@@ -1,23 +1,37 @@
 import { forwardRef } from "react";
+import { Link } from "react-router-dom";
+import { useCart } from "../../context/CardContext";
 
 const Sidenav = forwardRef(
   ({ showNav, categories, selectedCategory, onSelect }, ref) => {
+    const { state } = useCart();
+    const ItemsCount = state.cartItems.length;
+
     return (
       <aside ref={ref} className={showNav ? "sidenav active" : "sidenav"}>
-        <ul>
-          {categories.map((category) => (
-            <li key={category}>
-              <button
-                className={`category-btn ${
-                  selectedCategory === category ? "active" : ""
-                }`}
-                onClick={() => onSelect(category)}
-              >
-                {category}
-              </button>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h4>Categories</h4>
+          <ul>
+            {categories.map((category) => (
+              <li key={category}>
+                <button
+                  className={`category-btn ${
+                    selectedCategory === category ? "active" : ""
+                  }`}
+                  onClick={() => onSelect(category)}
+                >
+                  {category}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <h4>
+          <Link to="/cart"> Cart ({ItemsCount})</Link>
+        </h4>
+        <h4>
+          <Link to="/favorites">Favorites</Link>
+        </h4>
         {selectedCategory && (
           <button className="clear-btn" onClick={() => onSelect(null)}>
             Clear filters
